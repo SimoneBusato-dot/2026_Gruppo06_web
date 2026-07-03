07fd1a40b4ead15ba243fc62a977f37dfe678047
SimoneBusato-dot <simone.busato@mail.polimi.it>
2026-07-03 23:27:41 +0200

    sezione finale sistemata

--- src/lib/components/Card_Selection/considerations.svelte
+++ src/lib/components/Card_Selection/considerations.svelte
@@ -19,14 +19,14 @@
             gsap.set(lineEnding1, {strokeDasharray: lengthEd, strokeDashoffset: lengthEd,});
             gsap.set(lineEnding2, {strokeDasharray: lengthEd2, strokeDashoffset: lengthEd2, y: "-60%"});
 
-            const splitUpper = new SplitText(upperEnding, { type: "lines", mask: "lines" });
+            const splitUpper = new SplitText(upperEnding, { type: "lines", mask: "lines", linesClass: "split-line" });
             const upperEnter = gsap.fromTo(splitUpper.lines, { y: "120%"}, { y: "0%", duration: 0.6, stagger:0.1, ease:"elastic.out(0.5,0.4)", paused: true});
             const splitUpper2 = new SplitText(upperEnding2, { type: "lines", mask: "lines" });
             const upperEnter2 = gsap.fromTo(splitUpper2.lines, { y: "120%"}, { y: "0%", duration: 0.6, stagger:0.1, ease:"elastic.out(0.5,0.4)", paused: true});
             const splitSubtitle = new SplitText(subtitle1, { type: "lines", mask: "lines" });
-            const subtitleEnter = gsap.fromTo(splitSubtitle.lines, { y: "120%"}, { y: "0%", duration: 0.6,ease:"power2.out", paused: true, delay: 1.5});
+            const subtitleEnter = gsap.fromTo(splitSubtitle.lines, { y: "120%"}, { y: "0%", duration: 0.6,ease:"power2.out", paused: true, delay: 0.5});
             const splitSubtitle2 = new SplitText(subtitle2, { type: "lines", mask: "lines" });
-            const subtitleEnter2 = gsap.fromTo(splitSubtitle2.lines, { y: "120%"}, { y: "0%", duration: 0.6,ease:"power2.out", paused: true, delay: 1.5});
+            const subtitleEnter2 = gsap.fromTo(splitSubtitle2.lines, { y: "120%"}, { y: "0%", duration: 0.6,ease:"power2.out", paused: true, delay: 0.5});
             let tl = gsap.timeline({
                 scrollTrigger: {
                     trigger: "#firstEnding",
@@ -44,7 +44,23 @@
 
                 });
 
-            tl.to(lineEnding1, {strokeDashoffset: -lengthEd, duration: 2, ease: "power2.out",}, 0);
+                let tl12 = gsap.timeline({
+                scrollTrigger: {
+                    trigger: "#firstEnding",
+                    start: "top top",
+                    end: "+=400%",
+                    scrub: 1,
+                    onEnter() {upperEnter.play(); subtitleEnter.play();},
+                    onLeave() {upperEnter.reverse(); subtitleEnter.reverse();},
+                    onEnterBack() {upperEnter.play(); subtitleEnter.play();},
+                    onLeaveBack() {upperEnter.reverse(); subtitleEnter.reverse();}
+
+                    }
+
+                });
+
+
+            tl12.to(lineEnding1, {strokeDashoffset: -lengthEd, duration: 2, ease: "power2.out",}, 0);
 
 
             let tl2 = gsap.timeline({
@@ -63,7 +79,22 @@
                 }
         })
 
-        tl2.to(lineEnding2, {strokeDashoffset: -lengthEd2, duration: 2, ease: "power2.out"}, 0);
+        let tl22 = gsap.timeline({
+                scrollTrigger: {
+                    trigger: "#secondEnding",
+                    start: "top top",
+                    end: "+=300%",
+                    scrub: 1,
+                    onEnter() {upperEnter.play(); subtitleEnter.play();},
+                    onLeave() {upperEnter.reverse(); subtitleEnter.reverse();},
+                    onEnterBack() {upperEnter.play(); subtitleEnter.play();},
+                    onLeaveBack() {upperEnter.reverse(); subtitleEnter.reverse();}
+
+                    }
+
+                });
+
+        tl22.to(lineEnding2, {strokeDashoffset: -lengthEd2, duration: 2, ease: "power2.out"}, 0);
         })
 </script>
 
@@ -71,7 +102,7 @@
     <section id="firstEnding">
         <div id="svgContainer1">
             <svg width="169" height="820" viewBox="0 0 169 820" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path bind:this={lineEnding1} d="M398.298 812.015C343.02 619.523 62.6996 671.217 27.749 444.902C-0.46257 262.225 246.217 169.202 284.486 5.81536" stroke="#533EDC" stroke-width="51"/>
+                <path bind:this={lineEnding1} d="M398.298 812.015C343.02 619.523 62.6996 671.217 27.749 444.902C-0.46257 262.225 246.217 169.202 284.486 5.81536" stroke="#533EDC" stroke-width="51" stroke-linecap="round"/>
             </svg>
         </div>
         <div id="textContainer1">
@@ -82,7 +113,7 @@
     <section id="secondEnding">
         <div id="svgContainer2">
             <svg width="100vw" height="100vh" viewBox="0 0 169 820" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path bind:this={lineEnding2} d="M-382 1334.68C178.273 1404.65 213 1092 183.5 952.5C154 813 -35.5139 714.114 -105.814 602.163C-176.115 490.212 159.931 323.618 86.5836 160.525C11.3213 -6.82501 -220.183 28.6433 -220.183 28.6433" stroke="#DC3E41" stroke-width="51" stroke-linejoin="round"/>
+                <path bind:this={lineEnding2} d="M-382 1334.68C178.273 1404.65 213 1092 183.5 952.5C154 813 -35.5139 714.114 -105.814 602.163C-176.115 490.212 159.931 323.618 86.5836 160.525C11.3213 -6.82501 -220.183 28.6433 -220.183 28.6433" stroke="#DC3E41" stroke-width="51" stroke-linecap="round"/>
             </svg>
 
         </div>
@@ -95,6 +126,12 @@
 
 
 <style>
+
+    :global(.split-line) {
+        padding-block: 0.18em; /* spazio extra per gli accenti */
+        margin-block: -0.18em; /* annulla lo spostamento nel layout */
+        overflow: clip; /* invece di hidden, evita alcuni glitch di rendering su Safari */
+    }
  
     #firstEnding {
         display: flex;
